3626fadb1e11a2f8274fb46a75535ea0b9a1d476
METR1CKA <ferchosalazar054@gmail.com>
2024-10-05 21:47:14 -0600

    feat: server.js changed to original

--- server.js
+++ server.js
@@ -1,70 +1,41 @@
-const express = require('express')
-const constants = require('./app/utils/constants')
-const config = require('./config.json')
-const app = express()
-app.use(express.json())
+const express = require('express');
+const constants = require('./app/utils/constants');
+const config = require('./config.json');
+const app = express();
+app.use(express.json());
+app.use(express.static(
+    __dirname + '/app/public')
+);
 
-let entorno = constants.ENVIRONMENTS.production
-const args = process.argv
-
-if (args.length <= 2) {
-    console.log('Parametro --entorno es requerido')
-    process.exit(1)
-}
-
-args.forEach((argument) => {
-    const arg = argument.toLowerCase()
-
-    if (arg.startsWith('--entorno=')) {
-        let valor = arg.replace('--entorno=', '')
-        entorno =
-            constants.ENVIRONMENTS[valor] ?? constants.ENVIRONMENTS.production
+let entorno = constants.ENVIRONMENTS.production;
+const args = process.argv;
+args.forEach(argument => {
+    const arg = argument.toLowerCase();
+    if(arg.startsWith("--entorno=")){
+        let valor = arg.replace("--entorno=", '');
+        entorno = constants.ENVIRONMENTS[valor] 
+               || constants.ENVIRONMENTS.production;
     }
-})
+});
 
-app.post('/escuela', async (req, res) => {
-    const body = req.body || {}
-    const validate = require('./validation/escuela.validate')
-    const escuelaValida = validate.nuevaEscuela(body)
+const escuelasRouter = require('./app/routers/escuela');
+app.use('/escuela', escuelasRouter);
 
-    if (escuelaValida.error) {
-        return res.status(400).send(escuelaValida.error)
+const PORT = constants.PORTS[entorno];
+app.listen(PORT, function(error) {
+    if(error) {
+        console.log(error);
+        process.exit(0);
     }
 
-    const Escuela = require('./app/models/Escuela')
-    const nuevaEscuela = new Escuela(body)
-    await nuevaEscuela.save()
+    console.log(`Ejecutando en el puerto: ${PORT}`);
+    const mongoose = require('mongoose');
 
-    res.send({
-        ok: true,
-    })
-})
-
-app.get('/escuelas', async (req, res) => {
-    const Escuela = require('./app/models/Escuela')
-    const escuelas = await Escuela.find()
-    res.send(escuelas)
-})
-
-const PORT = constants.PORTS[entorno]
-
-app.listen(PORT, (error) => {
-    if (error) {
-        console.error(error)
-        process.exit(1)
-    }
-
-    console.log(`Ejecutando servidor en el puerto: ${PORT}`)
-    const mongoose = require('mongoose')
-
-    const mongoURL = `${config.mongo[entorno].host}/${config.mongo[entorno].defaultDB}`
-    mongoose
-        .connect(mongoURL)
-        .then(() => {
-            console.log(`Connected: ${mongoURL}`)
-        })
-        .catch((error) => {
-            console.error(error)
-            process.exit(1)
-        })
-})
+    const mongoURL = `${config.mongo[entorno].host}/${config.mongo[entorno].defaultDB}`;
+    mongoose.connect(mongoURL).then(() => {
+        console.log('Connected: ' + mongoURL);
+    }).catch(error => {
+        console.error(error);
+        process.exit(0);
+    });
+});
